e9747041a95c8a39d64889d03fb350a1fe3e8515
soumahoro hamed <hamedsoum4023@gmail.com>
2021-12-01 08:06:21 +0000

    first commit

---
+++ src/main/java/soum/userAdmin/UserAdminApplication.java
@@ -0,0 +1,13 @@
+package soum.userAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soum/userAdmin/UserAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package soum.userAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
